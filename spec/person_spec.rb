aec953bbf65c50645c6268a4bf1bd3d07dd9fa4c
Haridas <haridasnykiel@gmail.com>
2016-06-23 09:45:17 +0000

    Made test for person string, this passed. Also created files for the FamilMembers class

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe "Person class" do
+
+  before :each do
+    @person = Person.new "joe", "bloggs"
+  end
+
+
   it 'should store and return personal information' do
     person = Person.new("joe", "bloggs", "3 Feb 1992") # Testing the intialize method
 
@@ -11,46 +17,50 @@ describe "Person class" do
   end
 
   it "dob method should be optional" do
-    person = Person.new("joe", "bloggs")
-    expect(person.dob).to be_nil
+    # person = Person.new("joe", "bloggs")
+    expect(@person.dob).to be_nil
   end
 
   it "Should have emails as an array" do
-    person = Person.new("joe", "bloggs")
-    expect(person.email).to be_an(Array)
+    # person = Person.new("joe", "bloggs")
+    expect(@person.email).to be_an(Array)
   end
 
   it "Should have phone numbers as an array" do
-    person = Person.new("joe", "bloggs")
-    expect(person.phone_numbers).to be_an(Array)
+    # person = Person.new("joe", "bloggs")
+    expect(@person.phone_numbers).to be_an(Array)
   end
 
   it "Should be able to add elements to the email array" do
-    person = Person.new("joe", "bloggs")
-    person.add_email('hari@gmail.com')
-    expect(person.email).to eq ["hari@gmail.com"]
+    # person = Person.new("joe", "bloggs")
+    @person.add_email('hari@gmail.com')
+    expect(@person.email).to eq ["hari@gmail.com"]
   end
 
   it "Should be able to add elements to the phone numbers array" do
-    person = Person.new("joe", "bloggs")
-    person.add_phone("07878337982")
-    expect(person.phone_numbers).to eq ["07878337982"]
+    # person = Person.new("joe", "bloggs")
+    @person.add_phone("07878337982")
+    expect(@person.phone_numbers).to eq ["07878337982"]
   end
 
   it "Should be able to remove an element from the email array" do
-    person = Person.new("joe", "bloggs")
-    person.add_email('hari@gmail.com')
-    person.remove_email(0)
-    expect(person.email).to be_empty
+    # person = Person.new("joe", "bloggs")
+    @person.add_email('hari@gmail.com')
+    @person.remove_email(0)
+    expect(@person.email).to be_empty
   end
 
   it "Should be able to remove an element from the phone numbers array" do
-    person = Person.new("joe", "bloggs")
-    person.add_phone("07878337982")
-    person.remove_phone(0)
-    expect(person.phone_numbers).to be_empty
+    # person = Person.new("joe", "bloggs")
+    @person.add_phone("07878337982")
+    @person.remove_phone(0)
+    expect(@person.phone_numbers).to be_empty
   end
 
+  it "Should display a string in the correct format" do
+      person = Person.new("joe", "bloggs", "3 Feb 1992")
+      expect(person.person_s).to eq "Joe Bloggs was born on 1992-02-03.\n Their email addresses are:[].\n Their phone numbers are:[].\n"
+  end
 
 
 end
